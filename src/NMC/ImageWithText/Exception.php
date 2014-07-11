10404b029aac34563753f1f1d797889218b5d282
Younès El Biache <younes.elbiache@gmail.com>
2014-07-11 16:25:12 +0200

    added its own exception

---
+++ src/NMC/ImageWithText/Exception.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Image with Text
+ *
+ * @author      Josh Lockhart <josh@newmediacampaigns.com>
+ * @copyright   2013 Josh Lockhart
+ * @link        https://github.com/nmcteam/image-with-text
+ * @license     MIT
+ * @version     2.0.2
+ *
+ * MIT LICENSE
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+namespace NMC\ImageWithText;
+
+/**
+ * Exception
+ *
+ * @author Younès El Biache
+ * @since  1.0.0
+ */
+class Exception extends Exception {}
